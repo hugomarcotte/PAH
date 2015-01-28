0a8b6ee6b0a74f5d75837a26c7a6492b1cc13f47
Hugo Marcotte <marcotte.hugo@gmail.com>
2015-01-28 12:53:34 -0500

    Initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pahApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
